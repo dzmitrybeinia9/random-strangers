90ac8906409fdc7f582011fcf844293f39553ce3
dzmitrybeinia <dmitribeinia@gmail.com>
2024-11-19 08:56:39 +0100

    ref types

--- src/table/columns.tsx
+++ src/table/columns.tsx
@@ -1,19 +1,5 @@
 import { ColumnDef } from "@tanstack/react-table"
-
-export type TeamData = {
-    place: number
-    team: string
-    rank_id: number
-    rank_name: string
-    city: string
-    games: number
-    wins: number
-    points: number
-    season_games: number
-    season_wins: number
-    season_points: number
-    isChampion: number
-}
+import { TeamData } from "../lib/types"
 
 const SortableHeader = ({
     label,
