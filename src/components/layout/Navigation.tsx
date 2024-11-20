c93ab44af94706d5576ba089a93e230cfb8a65e1
dzmitrybeinia <dmitribeinia@gmail.com>
2024-11-20 17:14:42 +0100

    fix logo position

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -5,7 +5,7 @@ function Navigation() {
   const location = useLocation()
 
   const getLinkClass = (path: string) => `
-    inline-flex ml-6 items-center px-1 pt-1 border-b-2 text-base font-bold
+    inline-flex items-center px-1 pt-1 border-b-2 text-base font-bold
     ${location.pathname === path
       ? 'border-indigo-500 text-gray-900'
       : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
@@ -15,9 +15,9 @@ function Navigation() {
   return (
     <nav className="bg-white shadow-lg">
       <div className="max-w-7xl mx-auto px-1">
-        <div className="flex justify-between h-16">
+        <div className="flex h-16">
           <div className="flex">
-            <div className="flex-shrink-0 flex items-center">
+            <div className="h-16 w-16 flex pl-3 items-center">
               <img
                 className="h-8 w-8 rounded-full"
                 src={mzgbLogo}
